f59d1e22ef9d85adc6acfb72a88e977ac13271d9
Hans-Peter Locher <locher.hanspeter@gmail.com>
2009-05-28 13:18:09 +0000

    added interface for creating a Blog Entry

--- mailtoplone/scrawl/interfaces.py
+++ mailtoplone/scrawl/interfaces.py
@@ -22,9 +22,32 @@ __author__ = """Hans-Peter Locher <hans-peter.locher@inquant.de>"""
 __docformat__ = 'plaintext'
 
 
+from zope.interface import Interface
 from mailtoplone.base.interfaces import IMailDropBoxMarker
 
 class IScrawlMailDropBoxMarker(IMailDropBoxMarker):
     """ marker interface for Scrawl mail drop boxes """
+
+class IBlogEntryFactory(Interface):
+    """ create a Blog Entry
+
+    To create a blog entry use::
+
+        >>> factory = queryMultiAdapter((context, request), IBlogEntryFactory, default=Default(context, request))
+        >>> factory.add_category("email")
+        >>> factory.add_category("foo")
+        >>> factory.add_file("filedata", "text/plain", "sometext.txt")
+        >>> factory.add_file(imgdata, "image/jpeg", "foo.jpg")
+        >>> entry = factory.create("Some silly subject", "hey there, this is my text")
+
+    """
+    def create(title, text):
+        """ create the Blog Entry on the context and return it"""
+
+    def add_file(data, name, mime_type):
+        """ add a file to the Blog Entry """
+
+    def add_category(category):
+        """ add a category """
 
 # vim: set ft=python ts=4 sw=4 expandtab :
